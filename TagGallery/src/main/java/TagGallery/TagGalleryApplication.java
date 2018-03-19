c2441425665c71b7a4fcb0ab6cd2fc4edb4d50ba
GeorgeK95 <george_it@abv.bg>
2018-03-19 22:45:00 +0200

    Renamed class.

--- TagGallery/src/main/java/TagGallery/TagGalleryApplication.java
+++ TagGallery/src/main/java/TagGallery/TagGalleryApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class TagGalleryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
+        SpringApplication.run(TagGalleryApplication.class, args);
     }
 }
